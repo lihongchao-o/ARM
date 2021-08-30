c75b985470ac9e5e37871a085ba280f84c63194c
李宏超 <87232660+lihongchao-o@users.noreply.github.com>
2021-08-30 10:24:18 +0800

    KeyInput_IRQ_Timer
    Linux按键input子系统实验，含定时器、中断的应用。

---
+++ keyinput_irq_timer/keyinputApp.c
@@ -0,0 +1,83 @@
+/*
+ * @Author: lihongchao
+ * @Date: 2021-08-28 16:35:17
+ * @LastEditTime: 2021-08-30 10:20:54
+ * @LastEditors: your name
+ * @Description: input子系统测试APP。
+ * 使用方法	 ：./keyinputApp /dev/input/event1
+ * @FilePath: \keyinput_irq_timer\keyinputApp.c
+ */
+#include "stdio.h"
+#include "unistd.h"
+#include "sys/types.h"
+#include "sys/stat.h"
+#include "sys/ioctl.h"
+#include "fcntl.h"
+#include "stdlib.h"
+#include "string.h"
+#include <poll.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <linux/input.h>
+
+/* 定义一个input_event变量，存放输入事件信息 */
+static struct input_event inputevent;
+
+/*
+ * @description		: main主程序
+ * @param - argc 	: argv数组元素个数
+ * @param - argv 	: 具体参数
+ * @return 			: 0 成功;其他 失败
+ */
+int main(int argc, char *argv[])
+{
+	int fd;
+	int err = 0;
+	char *filename;
+
+	filename = argv[1];
+
+	if(argc != 2) {
+		printf("Error Usage!\r\n");
+		return -1;
+	}
+
+	fd = open(filename, O_RDWR);
+	if (fd < 0) {
+		printf("Can't open file %s\r\n", filename);
+		return -1;
+	}
+
+	while (1) {
+		err = read(fd, &inputevent, sizeof(inputevent));
+		if (err > 0) { /* 读取数据成功 */
+			switch (inputevent.type) {
+
+
+				case EV_KEY:
+					if (inputevent.code < BTN_MISC) { /* 键盘键值 */
+						printf("key %d %s\r\n", inputevent.code, inputevent.value ? "press" : "release");
+					} else {
+						printf("button %d %s\r\n", inputevent.code, inputevent.value ? "press" : "release");
+					}
+					break;
+
+				/* 其他类型的事件，自行处理 */
+				case EV_REL:
+					break;
+				case EV_ABS:
+					break;
+				case EV_MSC:
+					break;
+				case EV_SW:
+					break;
+			}
+		} else {
+			printf("读取数据失败\r\n");
+		}
+	}
+	return 0;
+}
+
